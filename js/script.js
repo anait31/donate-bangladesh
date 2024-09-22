89d52bffe2a41aa58253c13f11efaf7f6fecf824
anait07 <anaitullah198@gmail.com>
2024-09-22 23:40:01 +0800

    Calculate donation section

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+// Implementation Toggle Functionality
+
+// Toggle to History Section
+document.getElementById('toggle-history-btn').addEventListener('click', function () {
+    const toggleDonationButton = getElementValueById('toggle-donation-btn');
+    toggleDonationButton.classList.remove('bg-[#B4F461]');
+    toggleDonationButton.classList.add('border-2');
+
+    const toggleHistoryButton = getElementValueById('toggle-history-btn');
+    toggleHistoryButton.classList.add('bg-[#B4F461]');
+    toggleHistoryButton.classList.remove('border-2');
+
+    const allDonationCard = getElementValueById('all-donation-card');
+    allDonationCard.classList.add('hidden');
+
+    getElementValueById('donation-history').classList.remove('hidden')
+
+})
+
+// Toggle to Donation Section
+document.getElementById('toggle-donation-btn').addEventListener('click', function () {
+    const toggleDonationButton = getElementValueById('toggle-donation-btn');
+    toggleDonationButton.classList.add('bg-[#B4F461]');
+    toggleDonationButton.classList.remove('border-2');
+
+    const toggleHistoryButton = getElementValueById('toggle-history-btn');
+    toggleHistoryButton.classList.remove('bg-[#B4F461]');
+    toggleHistoryButton.classList.add('border-2');
+
+    const allDonationCard = getElementValueById('all-donation-card');
+    allDonationCard.classList.remove('hidden');
+    
+    getElementValueById('donation-history').classList.add('hidden')
+
+})
+
+
+
+// Implement Donation Functionality For Noakhali Flood
+document.getElementById('donation-btn').addEventListener('click', function() {
+    
+    const donationNoakhaliFlood = parseFloat(document.getElementById('donation-noakhali-flood').value);
+
+    let MyCurrentBalance = parseFloat(document.getElementById('my-available-balance').innerText);
+    console.log(typeof MyCurrentBalance);
+    let currentBalance = parseFloat(getElementValueById('current-balance').innerText);
+
+    if(MyCurrentBalance <  donationNoakhaliFlood || donationNoakhaliFlood < 0) {
+        return alert('Balance not available')
+    }
+    
+    getElementValueById('current-balance').innerText = currentBalance + donationNoakhaliFlood;
+
+    document.getElementById('my-available-balance').innerText = parseFloat(MyCurrentBalance) - parseFloat(donationNoakhaliFlood);
+
+})
+
---
+++ js/utilities.js
@@ -0,0 +1,7 @@
+function getElementValueById(id) {
+    return document.getElementById(id);
+}
+
+// function getInputValueById(id) {
+//     return document.getElementById(id).value;
+// }
