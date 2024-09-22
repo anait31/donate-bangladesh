4ae8886984955dae2b87d3e16606d0abaa0a7275
anait31 <anaitullah198@gmail.com>
2024-09-22 20:49:45 +0800

    Basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+    extend: {
+        // fontFamily: {
+        //     custom: ['Lexend', 'sans-serif'], // Fallback font
+        //   },
+    },
+};
+export const plugins = [];
