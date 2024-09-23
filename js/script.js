c1e69a65553dd0f2f5f89f05db417d4cedc6e761
anait31 <anaitullah198@gmail.com>
2024-09-23 15:21:00 +0800

    implementation relief donation section

--- js/script.js
+++ js/script.js
@@ -62,26 +62,48 @@ document.getElementById('donation-btn').addEventListener('click', function () {
 // Implement Donate for Flood Relief in Feni
 
 
+document.getElementById('relief-donation-btn').addEventListener('click', function () {
+
+    const donationReliefFlood = parseFloat(document.getElementById('donation-relief-field').value);
+
+    let MyCurrentBalance = parseFloat(document.getElementById('my-available-balance').innerText);
+
+    let currentBalance = parseFloat(getElementValueById('relief-current-balance').innerText);
+
+    if (MyCurrentBalance < donationReliefFlood || donationReliefFlood < 0 || isNaN(donationReliefFlood)) {
+        return alert('Invalid Donation Amount')
+    }
+
+    getElementValueById('relief-current-balance').innerText = currentBalance + donationReliefFlood;
+
+    document.getElementById('my-available-balance').innerText = parseFloat(MyCurrentBalance) - parseFloat(donationReliefFlood);
+
+    const modal = document.getElementById('my_modal_5');
+    modal.showModal()
+
+})
+
+
+// Implement Aid for Injured in the Quota Movement
+
 document.getElementById('injured-donation-btn').addEventListener('click', function () {
 
     const donationInjuredFlood = parseFloat(document.getElementById('donation-injured-field').value);
 
+    let currentBalance = parseFloat(getElementValueById('injured-donated-balance').innerText);
+
     let MyCurrentBalance = parseFloat(document.getElementById('my-available-balance').innerText);
 
-    let currentBalance = parseFloat(getElementValueById('injured-current-balance').innerText);
-
     if (MyCurrentBalance < donationInjuredFlood || donationInjuredFlood < 0 || isNaN(donationInjuredFlood)) {
-        return alert('Invalid Donation Amount')
+        return alert('Invalid Donation Amount');
     }
 
-    getElementValueById('injured-current-balance').innerText = currentBalance + donationInjuredFlood;
+    getElementValueById('injured-donated-balance').innerText = currentBalance + donationInjuredFlood;
+
 
     document.getElementById('my-available-balance').innerText = parseFloat(MyCurrentBalance) - parseFloat(donationInjuredFlood);
 
     const modal = document.getElementById('my_modal_5');
     modal.showModal()
-    
+
 })
-
-
-// Implement Aid for Injured in the Quota Movement
